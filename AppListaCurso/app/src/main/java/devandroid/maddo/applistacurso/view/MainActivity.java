c30889965f0fd6cf38823537a98a83c7bfc03fd2
Daniel <dani2014games@gmail.com>
2023-03-09 16:13:05 -0300

    New things here

--- AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/view/MainActivity.java
@@ -5,12 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import devandroid.maddo.applistacurso.R;
+import devandroid.maddo.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
+    Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Marco");
+        pessoa.setSobreNome("Maddo");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("11-22323996");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Daniel");
+        outraPessoa.setSobreNome("Borges");
+        outraPessoa.setCursoDesejado("Android");
+        outraPessoa.setTelefoneContato("11-73523543");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+
+        int parada = 0;
     }
 }
--- Soma2/app/src/main/java/com/example/soma/MainActivity.kt
+++ Soma2/app/src/main/java/com/example/soma/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.soma
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,7 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,13 +18,24 @@ class MainActivity : AppCompatActivity() {
         val txtResult:TextView=findViewById(R.id.txtResultado)
         val btnSoma:Button=findViewById(R.id.Somar)
         val btnMulti:Button=findViewById(R.id.Multiplicar)
+        val btnDiv:Button=findViewById(R.id.Divisao)
+        val btnSubtracao:Button=findViewById(R.id.Subtracao)
 
         btnSoma.setOnClickListener {
-            txtResult.text=(edNum1.text.toString().toInt() + edNum2.text.toString().toInt()).toString()
+            txtResult.text=(edNum1.text.toString().toFloat() + edNum2.text.toString().toFloat()).toString()
         }
 
         btnMulti.setOnClickListener {
-            txtResult.text=(edNum1.text.toString().toInt() * edNum2.text.toString().toInt()).toString()
+            txtResult.text=(edNum1.text.toString().toFloat() * edNum2.text.toString().toFloat()).toString()
         }
+
+        btnDiv.setOnClickListener {
+            txtResult.text=(edNum1.text.toString().toFloat() / edNum2.text.toString().toFloat()).toString()
+        }
+
+        btnSubtracao.setOnClickListener {
+            txtResult.text=(edNum1.text.toString().toFloat() - edNum2.text.toString().toFloat()).toString()
+        }
+
     }
 }
