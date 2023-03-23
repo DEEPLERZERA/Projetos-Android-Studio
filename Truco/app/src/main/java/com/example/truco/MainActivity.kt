a790ba1fe3be5b70fcdec10dd3a01f86237055a6
Daniel <dani2014games@gmail.com>
2023-03-23 00:39:07 -0300

    Adding new things

--- Truco/app/src/main/java/com/example/truco/MainActivity.kt
+++ Truco/app/src/main/java/com/example/truco/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.truco
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
-
+    //Variáveis globais
     var pontosTimeA:Int = 0
     var pontosTimeB:Int = 0
     lateinit var txtPlacarA:TextView
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        //Declaração de variáveis
         val pTimeA:TextView=findViewById(R.id.PlacartimeA)
         val pTimeB:TextView=findViewById(R.id.PlacartimeB)
         val btnTrucoA:Button=findViewById(R.id.btnTruco)
@@ -27,25 +29,55 @@ class MainActivity : AppCompatActivity() {
         val btnCorreuB:Button=findViewById(R.id.btnCorreu2)
         val btnReiniciar:Button=findViewById(R.id.btnReiniciar)
 
-        txtPlacarA = findViewById(R.id.PlacartimeA)
+        txtPlacarA = findViewById(R.id.PlacartimeA)  //Caixas de texto
         txtPlacarB = findViewById(R.id.PlacartimeB)
 
+        //Funções para cada botão
         btnTrucoA.setOnClickListener{adicionaPonto(3, "A")}
         btnTrucoB.setOnClickListener{adicionaPonto(3, "B")}
         btnCorreuA.setOnClickListener{adicionaPonto(1, "A")}
         btnCorreuB.setOnClickListener{adicionaPonto(1, "B")}
+        btnReiniciar.setOnClickListener{reinicia()}
 
 
 
     }
-    fun adicionaPonto(pontos:Int, time:String){
-        if(time=="A"){
-            pontosTimeA+=pontos
+    fun adicionaPonto(pontos:Int, time:String){  //Função de adicionar pontos
+        if(time=="A"){  //Condicional
+            pontosTimeA+=pontos  //Incrementador de pontos
             txtPlacarA.setText(pontosTimeA.toString())
+            if(pontosTimeA>=12){
+                vitoria("A")
+            }
         }else{
             pontosTimeB+=pontos
+            if(pontosTimeB>=12){
+                vitoria("B")
+            }
             txtPlacarB.setText(pontosTimeB.toString())
         }
+    }
+
+
+    fun vitoria(time:String){ //Criando função de declarar vitoria
+        val builder= AlertDialog.Builder(this) //Cria caixa de Alerta
+        builder.setTitle("Vencedor !!")   //Seta título na caixa
+        builder.setMessage("O Time " + time + " ganhou !!")  //Seta mensagem
+        builder.setPositiveButton(R.string.yes){dialog,which ->   //Cria botão
+            Toast.makeText(applicationContext, android.R.string.yes,
+            Toast.LENGTH_LONG).show()
+        }
+        builder.show()  //Constroi essa tela
+        reinicia() //Chamando func de reiniciar
+    }
+
+
+    fun reinicia(){ //Criando função de reiniciar
+       pontosTimeA=0
+       pontosTimeB=0
+       txtPlacarA.setText("0")  //Setando texto
+       txtPlacarB.setText("0")
+       Toast.makeText(this, "Placar Reiniciado !", Toast.LENGTH_LONG) //Falando que foi reiniciado
     }
 
 
